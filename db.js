8cf93bdc69f5b9808fc09cdb2934a67e98f5d12a
dangen-effy <dangen.effy@gmail.com>
2019-08-15 17:41:17 +0900

    rm db.js

--- db.js
+++
@@ -1,9 +0,0 @@
-const Datastore = require('nedb')
-const db = new Datastore({ filename: 'db/data', autoload: true })
-// You can issue commands right away
-
-db.replay = new Datastore('db/replay')
-
-db.insert([{ a: 5 }, { a: 42 }], function (_, newDoc) {
-  console.log(newDoc)
-})
